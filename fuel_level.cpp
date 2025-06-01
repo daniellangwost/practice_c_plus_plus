4f1cccd98be01f84cc4b7572e52b32cbfef1b374
LadanCode <70443440+LadanCode@users.noreply.github.com>
2025-06-01 15:16:06 +0200

    Update with control flow elements

--- fuel_level.cpp
+++ fuel_level.cpp
@@ -7,6 +7,12 @@ int main()
   double level{};
   std::cin >> level;
 
-  std::cout << "Fuel: " << level << " units\n";
+  if (level > 50) std::cout << "Ready to launch\n";
+  else std::cout << "Low fuel\n";
+
+  for (int i{ 1 }; i <= 3; ++i)
+  {
+    std::cout << "Checking system: " << i << "\n";
+  }
   return 0;
 }
