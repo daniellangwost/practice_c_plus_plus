6bd0cad97262b9b8a203926584a01d5cf92916fb
LadanCode <70443440+LadanCode@users.noreply.github.com>
2025-09-11 21:22:14 +0200

    Add remove()

--- algorithms_datastructures/binary_search_tree/binary_search_tree.cpp
+++ algorithms_datastructures/binary_search_tree/binary_search_tree.cpp
@@ -9,7 +9,7 @@ BinarySearchTree::BinarySearchTree()
 void BinarySearchTree::insert(int value, Node* start)
 {
   Node* curr = start;
-  if (curr->value == value) {return;}
+  if (curr->value == value) return;
 
   if (value < curr->value)
   {
@@ -51,23 +51,115 @@ void BinarySearchTree::insert(int value)
   insert(value, root);
 }
 
-bool BinarySearchTree::search(int value, Node* start)
+bool BinarySearchTree::search(int value, Node* start) const
 {
   if (value == start->value) {return true;}
   if (value < start->value)
   {
-    if (!start->left) {return false;}
+    if (!start->left) return false;
     return search(value, start->left);
   } else if (value > start->value)
   {
-    if (!start->right) {return false;}
+    if (!start->right) return false;
     return search(value, start->right);
   }
   return false;
 }
 
-bool BinarySearchTree::search(int value)
+bool BinarySearchTree::search(int value) const
 {
-  if (!root) {return false;}
+  if (!root) return false;
   return search(value, root);
+}
+
+bool BinarySearchTree::remove(int value)
+{
+  Node* curr = root;
+  Node* parent = nullptr;
+  bool isLeftChild;
+  while (curr)
+  {
+    if (curr->value == value) {break;}
+    
+    if (value < curr->value)
+    {
+      if (!curr->left) return false; // value does not exist
+      parent = curr;
+      isLeftChild = true;
+      curr = curr->left;
+    } 
+    else if (value > curr->value)
+    {
+      if (!curr->right) return false; // value does not exist
+      parent = curr;
+      isLeftChild = false;
+      curr = curr->right;
+    }
+  }
+
+  if (!curr) return false;
+
+  // curr points to the Node to be removed
+
+  if (!curr->left && !curr->right) // leaf node
+  {
+    if (curr == root)
+    {
+      root = nullptr;
+    }
+    else
+    {
+      if (isLeftChild) parent->left = nullptr;
+      else parent->right = nullptr;
+    }
+    delete curr;
+  } 
+  else if ((!curr->left && curr->right) || (curr->left && !curr->right)) // one child node
+  {
+    Node* child = curr->left ? curr->left : curr->right;
+
+    if (curr == root) 
+    {
+      root = child;
+    }
+    else
+    {
+      if (isLeftChild) parent->left = child;
+      else parent->right = child;
+    }
+    delete curr;
+  }
+  else // two child nodes
+  {
+    // find smallest in right subtree
+    Node* successor = curr->right;
+    Node* successor_parent = curr;
+    while (successor->left)
+    {
+      successor_parent = successor;
+      successor = successor->left;
+    }
+
+    curr->value = successor->value; // replace with successor
+
+    if (!successor->left && !successor->right)
+    {
+      if (successor_parent == curr) curr->right = nullptr;
+      else successor_parent->left = nullptr;
+    }
+    else
+    {
+      if (successor_parent == curr)
+      {
+        successor_parent->right = successor->right;
+      }
+      else
+      {
+        successor_parent->left = successor->right; // remove successor from original position
+      }
+    }
+    delete successor;
+  }
+
+  return true;
 }
--- algorithms_datastructures/binary_search_tree/main.cpp
+++ algorithms_datastructures/binary_search_tree/main.cpp
@@ -4,25 +4,16 @@
 int main()
 {
   BinarySearchTree t;
-  t.insert(4);
 
-  t.insert(6);
-  t.insert(2);
+  t.insert(10);
+  t.insert(40);
+  t.insert(30);
+  t.insert(25);
 
-  t.insert(3);
-  t.insert(5);
-  t.insert(7);
-  t.insert(1);
+  std::cout << t.remove(30);
+  std::cout << t.remove(10);
+  std::cout << t.remove(25);
+  std::cout << t.remove(40);
 
-  std::cout << t.search(1) << "\n";
-  std::cout << t.search(2) << "\n";
-  std::cout << t.search(3) << "\n";
-  std::cout << t.search(4) << "\n";
-  std::cout << t.search(5) << "\n";
-  std::cout << t.search(6) << "\n";
-  std::cout << t.search(7) << "\n";
-  std::cout << t.search(8) << "\n";
-  std::cout << t.search(-1) << "\n";
-  std::cout << t.search(100) << "\n";
   std::cout << "Done";
 }
